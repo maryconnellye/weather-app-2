3c73a01ce9b2ef3c4ef7f46d7d63aebfb40f0deb
Mary Connelly <maryconnellye@gmail.com>
2025-03-13 15:30:11 -0500

    data address is reading correctly

--- script.js
+++ script.js
@@ -1,25 +1,27 @@
 
 async function getWeather(location) {
 
-try {
-	const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=us&include=days%2Chours%2Calerts&key=JK237L99V62XFJJWBQH4UWVG7&contentType=json`, {
-		mode: 'cors',
-		method: 'GET'
-	});
-	const data = await response.json();
-	console.log(data);
-
-} catch(error) {
-	console.log("Error fetching data", error);
-}
-}
-
-const locationInput = document.querySelector('#location');
-	document.body.appendChild(locationInput);
-
-const submit = document.querySelector('#submit');
-	document.body.appendChild(submit);
-	submit.addEventListener('click', () => {
-
-		getWeather(`${locationInput.value}`);	
-	});
+	try {
+		const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=us&include=days%2Chours%2Calerts&key=JK237L99V62XFJJWBQH4UWVG7&contentType=json`, {
+			mode: 'cors',
+			method: 'GET'
+		});
+		const data = await response.json();
+			console.log(data);
+		const addressData = data.address;
+			console.log(addressData);
+	
+	} catch(error) {
+		console.log("Error fetching data", error);
+	}
+	}
+	
+	const locationInput = document.querySelector('#locationInput');
+		document.body.appendChild(locationInput);
+	
+	const submit = document.querySelector('#submit');
+		document.body.appendChild(submit);
+		submit.addEventListener('click', () => {
+	
+			getWeather(`${locationInput.value}`);	
+		});
